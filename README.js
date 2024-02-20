4537b3d728a65bb9a707ddb8ead8cba1e81229d9
Ali Ahmadi <160420584+ali-ahmadi-1384@users.noreply.github.com>
2024-02-20 03:56:01 -0800

    Create README.js
    first bio

---
+++ README.js
@@ -0,0 +1,5 @@
+const AliAhmadi = {
+    pronouns: "he" | "man",
+    languages: [JAVASCRIPT, PYTHON, HTML, CSS],
+    message: "Everyone should learn how to code because programming teaches you how to think."
+}
